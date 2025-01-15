2e5323b6e7215ce185aeefe4dbc29307a095526c
Kazioslaw <Kamil_d13@wp.pl>
2025-01-15 13:57:05 +0100

    Created base models

---
+++ EquipmentHostingHub/Models/EquipementContract.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentHostingHub.Models
+{
+	public class EquipementContract
+	{
+		[Key]
+		public int ID { get; set; }
+		[Required]
+		[Display(Name = "Contractor name")]
+		public string Name { get; set; }
+		public int Quantity { get; set; }
+		public int ProcessEquipementID { get; set; }
+		public ProcessEquipement ProcessEquipement { get; set; }
+
+		public int ProductionFacilityID { get; set; }
+		public ProductionFacility ProductionFacility { get; set; }
+	}
+}
---
+++ EquipmentHostingHub/Models/ProductionFacility.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentHostingHub.Models
+{
+	public class ProductionFacility
+	{
+		[Key]
+		public int ID { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public decimal Area { get; set; }
+	}
+}
